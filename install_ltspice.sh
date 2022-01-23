53316c6654c35574bc593474e79325e70121e975
Kali <asvamogh@gmail.com>
2022-01-23 05:24:53 -0500

    Updated

---
+++ install_ltspice.sh
@@ -0,0 +1,7 @@
+#Lt Spice installer by CABREX (https://github.com/0xcabrex)
+
+# Install Ltspice and wine
+sudo apt install wine
+wget http://ltspice.linear-tech.com/software/LTspice64.exe
+wine LTspice64.exe
+rm LTspice64.exe
