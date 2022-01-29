97dc68127a6ce20e7b51227b31e3d2a6909a0d48
Kali <asvamogh@gmail.com>
2022-01-29 08:38:55 -0500

    Added error handling

--- install_ltspice.sh
+++ install_ltspice.sh
@@ -1,7 +1,24 @@
 #Lt Spice installer by CABREX (https://github.com/0xcabrex)
 
+# Function to catch keyboard interrupt
+ctrl_c() {
+        echo -e "\nKeyboard interrupt detected, Cleaning up..."
+        rm LTspice64.exe*
+        exit 2
+}
+
+trap "ctrl_c" 2
+
 # Install Ltspice and wine
 sudo apt install wine
 wget http://ltspice.linear-tech.com/software/LTspice64.exe
+echo 
+echo "Starting ltspice installation..."
 wine LTspice64.exe
 rm LTspice64.exe
+
+echo "Program completed"
+
+
+
+
