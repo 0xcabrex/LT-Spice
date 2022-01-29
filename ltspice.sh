fa87f9a4a090e8fa6ba1c379fc83d05cd32bce73
Kali <asvamogh@gmail.com>
2022-01-29 08:39:12 -0500

    Added some error handling

--- ltspice.sh
+++ ltspice.sh
@@ -1,1 +1,10 @@
-wine ~/.wine/drive_c/Program\ Files/LTC/LTspiceXVII/XVIIx64.exe
+# Part of Lt Spice installer by CABREX (https://github.com/0xcabrex)
+
+
+if [ -d "$HOME/.wine/drive_c/Program Files/LTC/LTspiceXVII" ]
+then
+	echo "Running LTspice...."
+	wine ~/.wine/drive_c/Program\ Files/LTC/LTspiceXVII/XVIIx64.exe &
+else
+	echo "LTspice is not installed, please install it: https://github.com/0xcabrex/LT-Spice"
+fi
